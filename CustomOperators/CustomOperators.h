e120de66f666c550969c64a43984993094153661
mohammadraziei <mohammadraziei1375@gmail.com>
2020-08-07 16:54:33 +0430

    upload project

---
+++ CustomOperators/CustomOperators.h
@@ -0,0 +1,254 @@
+#ifndef CUSTOMOPERATORS_H
+#define CUSTOMOPERATORS_H
+
+#include <complex>
+#include <stdexcept>
+#include <type_traits>
+#include <vector>
+
+#define _lengthError(x1, x2)                                                   \
+  if (x1.size() != x2.size())                                                  \
+    throw std::length_error("not match size");
+
+//////// operator +
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator+(const std::complex<T1> &a, T2 b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) + static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator+(T1 a, const std::complex<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) + static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator+(const std::vector<T1> &a, const std::vector<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  _lengthError(a, b);
+  size_t vecSize = a.size();
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] + b[i];
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator+(const std::vector<T1> &a, const T2 &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] + bb;
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator+(const T2 &b, const std::vector<T1> &a) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = bb + a[i];
+  return v;
+}
+
+template <class T>
+inline std::vector<T> &operator+=(std::vector<T> &a, const T &b) {
+  size_t vecSize = a.size();
+  for (size_t i = 0; i < vecSize; ++i)
+    a[i] += b;
+  return a;
+}
+
+//////// operator -
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator-(std::complex<T1> a, T2 b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) - static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator-(T1 a, std::complex<T2> b) {
+  typedef typename std::common_type<T1, T2>::type T;
+
+  return static_cast<std::complex<T>>(a) - static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator-(const std::vector<T1> &a, const std::vector<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  _lengthError(a, b);
+  size_t vecSize = a.size();
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] - b[i];
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator-(const std::vector<T1> &a, const T2 &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] - bb;
+  return v;
+}
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator-(const T2 &b, const std::vector<T1> &a) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = bb - a[i];
+  return v;
+}
+
+template <class T>
+inline std::vector<T> &operator-=(std::vector<T> &a, const T &b) {
+  size_t vecSize = a.size();
+  for (size_t i = 0; i < vecSize; ++i)
+    a[i] -= b;
+  return a;
+}
+
+//////// operator *
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator*(const std::complex<T1> &a, T2 b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) * static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator*(T1 a, const std::complex<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+
+  return static_cast<std::complex<T>>(a) * static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator*(const std::vector<T1> &a, const std::vector<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  _lengthError(a, b);
+  size_t vecSize = a.size();
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] * b[i];
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator*(const std::vector<T1> &a, const T2 &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] * bb;
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator*(const T2 &b, const std::vector<T1> &a) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = bb * a[i];
+  return v;
+}
+
+template <class T>
+inline std::vector<T> &operator*=(std::vector<T> &a, const T &b) {
+  size_t vecSize = a.size();
+  for (size_t i = 0; i < vecSize; ++i)
+    a[i] *= b;
+  return a;
+}
+
+//////// operator /
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator/(const std::complex<T1> &a, T2 b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) / static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::complex<typename std::common_type<T1, T2>::type>
+operator/(T1 a, const std::complex<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  return static_cast<std::complex<T>>(a) / static_cast<std::complex<T>>(b);
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator/(const std::vector<T1> &a, const std::vector<T2> &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  _lengthError(a, b);
+  size_t vecSize = a.size();
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] / b[i];
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator/(const std::vector<T1> &a, const T2 &b) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = a[i] / bb;
+  return v;
+}
+
+template <class T1, class T2>
+inline std::vector<typename std::common_type<T1, T2>::type>
+operator/(const T2 &b, const std::vector<T1> &a) {
+  typedef typename std::common_type<T1, T2>::type T;
+  size_t vecSize = a.size();
+  T bb = static_cast<T>(b);
+  std::vector<T> v(vecSize);
+  for (size_t i = 0; i < vecSize; ++i)
+    v[i] = bb / a[i];
+  return v;
+}
+
+template <class T>
+inline std::vector<T> &operator/=(std::vector<T> &a, const T &b) {
+  size_t vecSize = a.size();
+  for (size_t i = 0; i < vecSize; ++i)
+    a[i] /= b;
+  return a;
+}
+
+#endif // CUSTOMOPERATORS_H
---
+++ CustomOperators/main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+
+#include <iostream>
+#include <vector>
+
+#include "CustomOperators.h"
+
+#define show(x)                                                                \
+  std::cout << ">> " << #x << " :\n\t";                                        \
+  print(x);
+template <typename T> void print(const std::vector<T> &a) {
+  std::copy(a.begin(), a.end(), std::ostream_iterator<T>(std::cout, ", "));
+  std::cout << std::endl;
+}
+template <typename T> void print(const T &a) { std::cout << a << std::endl; }
+
+///////////////////////////////////////////////////////
+
+int main(int argc, char *argv[]) {
+  QCoreApplication app(argc, argv);
+  std::vector<int> a{1, 2, 3};
+  std::vector<std::complex<float>> b{0.5, 0.5, 0.5};
+  show(a);
+  show(b);
+  show(a + b);
+  show(a);
+  show(a[1]);
+  return app.exec();
+}
